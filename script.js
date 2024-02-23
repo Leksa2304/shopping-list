427160ba4eeff6daf62ebb563cb331acefbdb1ef
Elena Lem <151932734+Leksa2304@users.noreply.github.com>
2024-02-23 13:27:49 +0300

    Создан список покупок
    с  добавлением пункта по Entery

--- script.js
+++ script.js
@@ -7,3 +7,39 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const inputWrapper = document.querySelector('.input-wrapper'); //оболочка поля
+const input = document.querySelector('#input'); //поле ввода
+const itemsСontainer = document.querySelector('.items'); //контейнер для пунктов
+
+
+function newItem() {
+
+    const inputText = input.value;
+
+    const item = document.createElement('div');
+    item.classList.add('items');
+    item.textContent = inputText;
+
+    if (inputText != '') {
+
+        itemsСontainer.prepend(item);
+    }
+
+    input.value = '';
+
+    item.addEventListener('click', function() {
+
+        item.classList.toggle('done');
+    });
+
+};
+
+
+function byEnter(event) {
+    if (event.key == 'Enter') {
+        newItem();
+    }
+}
+
+input.addEventListener('keydown', byEnter);
