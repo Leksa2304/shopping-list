b3a2663ac532ac0fe3d18d23797ba8fa89664f53
Elena Lem <151932734+Leksa2304@users.noreply.github.com>
2024-02-24 19:20:31 +0300

    доработка задания
    перенос обработчика до очистки поля ввода и удаление второго пробного кода. Передеплоила с рабочей ветки.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 /* Очищать input после добавления нового элемента в список */
 
-const inputWrapper = document.querySelector('.input-wrapper'); //оболочка поля
+
 const input = document.querySelector('#input'); //поле ввода
 const itemsСontainer = document.querySelector('.items'); //контейнер для пунктов
 
@@ -20,49 +20,11 @@ function newItem() {
     const item = document.createElement('div');
     item.classList.add('items');
     item.textContent = inputText;
-
-    if (inputText != '') {
-
-        itemsСontainer.prepend(item);
-    }
-
-    input.value = '';
-
     item.addEventListener('click', function() {
 
         item.classList.toggle('done');
     });
 
-};
-
-
-function byEnter(event) {
-    if (event.key == 'Enter') {
-        newItem();
-    }
-}
-
-input.addEventListener('keydown', byEnter);
-
-
-/* Зачеркиваются не все пункты, которые добавлены????
-const inputWrapper = document.querySelector('.input-wrapper'); //оболочка поля
-const input = document.querySelector('#input'); //поле ввода
-const itemsСontainer = document.querySelector('.items'); //контейнер для пунктов
-
-
-function newItem() {
-
-    const inputText = input.value;
-
-    const item = document.createElement('div');
-    item.classList.add('newGrocery');
-
-
-
-
-    item.textContent = inputText;
-
     if (inputText != '') {
 
         itemsСontainer.prepend(item);
@@ -70,18 +32,7 @@ function newItem() {
 
     input.value = '';
 
-    const grocery = document.querySelectorAll('.newGrocery');
-
-    for (let elem of grocery) {
-
-        elem.addEventListener('click', function() {
-            elem.classList.toggle('done');
-        });
-    }
-
-
 };
-
 
 function byEnter(event) {
     if (event.key == 'Enter') {
@@ -90,4 +41,3 @@ function byEnter(event) {
 }
 
 input.addEventListener('keydown', byEnter);
-*/
